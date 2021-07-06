e7e7bc8e13ff2ef574698be260432de916c64f69
Jaacu <51016311+Jaacu1@users.noreply.github.com>
2021-07-06 08:52:54 +0200

    Dodaj pliki projektów.

---
+++ MVC_Design/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Design.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Kroniki_Krolobojcy()
+        {
+            return View();
+        }
+
+        public ActionResult Kingkiller_chronicles()
+        {
+            return View();
+        }
+
+        public ActionResult Opowiesc_o_kamratach()
+        {
+            return View();
+        }
+
+        public ActionResult Tales_of_the_kin()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Polityka_prywatnosci()
+        {
+            return View();
+        }
+
+        public ActionResult Privacy_policy()
+        {
+            return View();
+        }
+    }
+}
